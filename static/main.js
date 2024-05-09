9b922835db1cc9826b2768348ac8b4def346bc99
DarkAngel <marcelo@darkblue.tv>
2024-05-09 01:41:19 +0000

    html of beat chaser

--- static/main.js
+++ static/main.js
@@ -6,3 +6,52 @@ var socket = io.connect(window.location.origin);
         });
     });
 })();
+
+// Song Chaser
+var squares = document.querySelectorAll('#chaser-plan .beat');
+var chaser_timeout = undefined;
+var chaser = {
+    bpm: 120,
+    current: 0,
+    _timeout: undefined,
+    start: function() {
+        this.movenext();
+        var interval = (60000 / this.bpm); // Convert BPM to ms
+        chaser_timeout = setInterval(() =>{
+            this.movenext();
+        }, interval);
+    },
+    stop: function() {
+        clearInterval(chaser_timeout);
+    },
+    reset: function() {
+        squares.forEach((x)=> {
+            x.classList.remove('active');
+        });
+        this.current = 0;
+    },
+    movenext: function() {
+        
+        // show next square
+        squares.forEach(x=>x.classList.remove('active'));
+        squares[chaser.current].classList.add('active');
+
+        // set next square
+        this.current = (this.current + 1) % squares.length;
+        if (this.current > squares.length) {this.current=0};
+
+    }
+}
+
+var chaser_start_button = document.querySelector('#chaser-start');
+var chaser_stop_button = document.querySelector('#chaser-stop');
+var chaser_reset_button = document.querySelector('#chaser-reset');
+chaser_start_button.addEventListener('click', ()=> {
+    chaser.start();
+});
+chaser_stop_button.addEventListener('click', ()=> {
+    chaser.stop();
+});
+chaser_reset_button.addEventListener('click', ()=> {
+    chaser.reset();
+});
