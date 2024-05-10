e94655465d71418a9f64bcbd85379f5fb7437d3f
DarkAngel <marcelo@darkblue.tv>
2024-05-10 01:42:42 +0000

    javascript chaser as ES6 module

---
+++ static/chaser.js
@@ -0,0 +1,47 @@
+// Beats Chaser
+export default class Chaser {
+    constructor({
+        bpm, 
+        s_beat
+    }) {
+        this.current = 0;
+        this.s_beat = s_beat;
+        this.bpm = bpm;
+        this.chase_timer = null;
+    }
+
+    start() {
+
+        this.chase_timer = setInterval(() => this.movenext(), 60000 / this.bpm);
+    }
+
+    stop() {
+        clearInterval(this.chase_timer);
+    }
+
+    reset() {
+        this.s_beat.forEach((x) => {
+            x.classList.remove('active');
+        });
+        this.current = 0;
+    }
+
+    // set the tempo in bpm
+    setBpm(newBpm) {
+        this.bpm = newBpm;
+        if (this.chase_timer) {
+            clearInterval(this.chase_timer);
+            this.start();
+        }
+    }
+
+    movenext() {
+        // show next square beat
+        this.s_beat.forEach(x => x.classList.remove('active'));
+        this.s_beat[this.current].classList.add('active');
+
+        // set next square beat
+        this.current = (this.current + 1) % this.s_beat.length;
+        if (this.current > this.s_beat.length) { this.current = 0; };
+    }
+}
--- static/main.js
+++ static/main.js
@@ -1,3 +1,5 @@
+import Chaser from './chaser.js';
+
 var socket = io.connect(window.location.origin);
 (function() {
     document.querySelectorAll('input.slider').forEach(function(x) {
@@ -5,64 +7,19 @@ var socket = io.connect(window.location.origin);
             socket.emit('slider_change', {channel_id: e.target.name, value: e.target.value});
         });
     });
+
+    // setup chaser blocks and controls
+    const chaser_beats = document.querySelectorAll('#chaser-plan .beat')
+    const chaser = new Chaser( {bpm:100, s_beat:chaser_beats });
+
+    document.querySelector('#chaser-start').addEventListener('click', ()=> {
+        chaser.start();
+    });
+    document.querySelector('#chaser-stop').addEventListener('click', ()=> {
+        chaser.stop();
+    });
+    document.querySelector('#chaser-reset').addEventListener('click', ()=> {
+        chaser.reset();
+    });
+
 })();
-
-// Song Chaser
-class Chaser {
-    constructor(bpm = 120, s_beat = []) {
-        this.current = 0;
-        this.s_beat = Array.from(s_beat); 
-        this.bpm = bpm;
-        this.chase_timer = null;
-    }
-
-    start() {
-        this.chase_timer = setInterval(() => this.movenext(), 60000 / this.bpm);
-    }
-
-    stop() {
-        clearInterval(this.chase_timer);
-    }
-
-    reset() {
-        this.s_beat.forEach((x) => {
-            x.classList.remove('active');
-        });
-        this.current = 0;
-    }
-
-    // set the tempo in bpm
-    setBpm(newBpm) {
-        this.bpm = newBpm;
-        if (this.chase_timer) {
-            clearInterval(this.chase_timer);
-            this.start();
-        }
-    }
-
-    movenext() {
-        // show next square beat
-        this.s_beat.forEach(x => x.classList.remove('active'));
-        this.s_beat[this.current].classList.add('active');
-
-        // set next square beat
-        this.current = (this.current + 1) % this.s_beat.length;
-        if (this.current > this.s_beat.length) { this.current = 0 };
-    }
-}
-
-var chaser_timeout = undefined;
-const chaser = new Chaser(
-    bpm = 120,
-    s_beat = document.querySelectorAll('#chaser-plan .beat')
-);
-
-document.querySelector('#chaser-start').addEventListener('click', ()=> {
-    chaser.start();
-});
-document.querySelector('#chaser-stop').addEventListener('click', ()=> {
-    chaser.stop();
-});
-document.querySelector('#chaser-reset').addEventListener('click', ()=> {
-    chaser.reset();
-});
